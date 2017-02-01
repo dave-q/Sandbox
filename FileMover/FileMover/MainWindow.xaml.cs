c5fd4e24fbd9dfb147846ebf0ce6e2746293df8b
Dave Quinn <daveq@idonix.com>
2017-02-01 23:17:56 +0000

    Basic UI sorted

--- FileMover/FileMover/MainWindow.xaml.cs
+++ FileMover/FileMover/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace FileMover
 {
@@ -23,6 +25,29 @@ namespace FileMover
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SourceFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void ChooseDestination_Click(object sender, RoutedEventArgs e)
+        {
+            var selectFolderDialog = new FolderBrowserDialog();
+            if (selectFolderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DestinationPath.Text = selectFolderDialog.SelectedPath;
+            }
         }
     }
 }
